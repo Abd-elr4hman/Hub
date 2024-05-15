24caa41a439f95500f51dc6e03b62c4228329631
Abd-elr4hman <ab.hassanein@gmail.com>
2024-05-15 23:05:27 +0300

    Added transitions to article and courses loading pages.

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -6,6 +6,7 @@ import VideoPlayer from "./_components/VideoPlayer";
 import CourseProgress from "@/components/CourseProgress";
 import CourseProgressButton from "./_components/CourseProgressButton";
 import Preview from "@/components/Preview";
+import Animate from "@/components/Animate";
 
 const ChapterIdPage = async ({
   params,
@@ -31,40 +32,45 @@ const ChapterIdPage = async ({
   const completeOnEnd = userProgress?.isCompeted;
 
   return (
-    <div className=" ">
-      {userProgress?.isCompeted && (
-        <Banner variant="success" label="You already completed this chapter" />
-      )}
-      {isLocked && <Banner variant="warning" label="This part is locked" />}
-
-      <div className="flex flex-col pb-10 max-w-7xl mx-auto">
-        <div className="p-4">
-          <VideoPlayer
-            chapterId={params.chapterId}
-            title={chapter.title}
-            courseId={params.courseId}
-            nextChapterId={nextChapter?.id}
-            playbackId={muxData?.playbackId!}
-            isLocked={isLocked}
-            completeOnEnd={completeOnEnd!}
+    <Animate>
+      <div className=" ">
+        {userProgress?.isCompeted && (
+          <Banner
+            variant="success"
+            label="You already completed this chapter"
           />
-        </div>
-        <div className="bg-slate-200 rounded-xl mx-6">
-          <div className="pt-2 px-4 flex flex-col lg:flex-row items-center justify-between">
-            <h2 className="text-2xl font-semibold mb-2">{chapter.title}</h2>
-            <CourseProgressButton
+        )}
+        {isLocked && <Banner variant="warning" label="This part is locked" />}
+
+        <div className="flex flex-col pb-10 max-w-7xl mx-auto">
+          <div className="p-4">
+            <VideoPlayer
               chapterId={params.chapterId}
+              title={chapter.title}
               courseId={params.courseId}
               nextChapterId={nextChapter?.id}
-              isCompeted={!!userProgress?.isCompeted}
+              playbackId={muxData?.playbackId!}
+              isLocked={isLocked}
+              completeOnEnd={completeOnEnd!}
             />
           </div>
-          <div className="pb-2 px-4">
-            <Preview value={chapter.description!} />
+          <div className="bg-slate-200 rounded-xl mx-6">
+            <div className="pt-2 px-4 flex flex-col lg:flex-row items-center justify-between">
+              <h2 className="text-2xl font-semibold mb-2">{chapter.title}</h2>
+              <CourseProgressButton
+                chapterId={params.chapterId}
+                courseId={params.courseId}
+                nextChapterId={nextChapter?.id}
+                isCompeted={!!userProgress?.isCompeted}
+              />
+            </div>
+            <div className="pb-2 px-4">
+              <Preview value={chapter.description!} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Animate>
   );
 };
 
