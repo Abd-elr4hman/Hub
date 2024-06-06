aa5029a8808ac2bf365c87e434a0c6d72597cb7f
Abdelrahman <ab.hassanein@gmail.com>
2024-06-06 14:24:16 +0300

    added supabase vector storage for article body encoding on creation.

--- app/api/articles/[articleId]/route.ts
+++ app/api/articles/[articleId]/route.ts
@@ -1,6 +1,8 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import { createClient } from "@/lib/supabase/server"; 
+import { getEmbedding } from "@/lib/utils";
 
 export async function PATCH(
   req: Request,
@@ -8,6 +10,7 @@ export async function PATCH(
 ) {
   try {
     const { userId } = auth();
+    const supabase = createClient()
     const { articleId } = params;
     const values = await req.json();
 
@@ -23,6 +26,34 @@ export async function PATCH(
         ...values,
       },
     });
+    
+    // TODO:
+    // if values contain "body" do the following:
+    if (values.body){
+      try {
+        // Create an openai embedding
+        const embedding = await getEmbedding(values.body)
+
+        // Check if the article already has an embedding for its body
+        // patch/create the embedding in supabase
+        const { data, error } = await supabase.from("article")
+        .upsert({
+          embedding: embedding,
+          prisma_id: articleId
+        }, { onConflict: 'prisma_id' })
+        .select()
+
+        if (error) {
+          console.log("[ARTICLE_ID_PATCH]", error)
+          return new NextResponse("Internal Error", { status: 500 });
+        }
+
+      } catch (err){
+        console.log("[ARTICLE_ID_PATCH]", err)
+        return new NextResponse("Internal Error", { status: 500 });
+      }
+    }
+
     return NextResponse.json(article);
   } catch (error) {
     console.log("[ARTICLE_ID_PATCH]", error);
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,35 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import OpenAI from "openai";
+import { NextResponse } from "next/server";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function getEmbedding(body:string) {
+  try {
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY
+    });
+  
+    if (!openai) {
+      return new NextResponse("Failed To Create Embedding", { status: 401 });
+    }
+  
+    const response = await openai.embeddings.create({
+      model: "text-embedding-ada-002",
+      input: body
+    })
+    
+    if (!response) {
+      return new NextResponse("Failed To Create Embedding", { status: 401 });
+    }
+  
+    return response.data[0].embedding
+  } catch (error) {
+    console.log("[UTILS_GET_EMBEDDING]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+  
 }
