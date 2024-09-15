fc536200b633c9de6dbd45827d2b3d4d4bba73b0
Abdelrahman <ab.hassanein@gmail.com>
2024-09-15 13:16:44 +0300

    Changed model Type

--- app/api/articles/[articleId]/chat/route.ts
+++ app/api/articles/[articleId]/chat/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
 
     // Request the OpenAI-compatible API for the response based on the prompt
     const response = await perplexity.chat.completions.create({
-      model: "llama-3-sonar-small-32k-chat",
+      model: "llama-3.1-sonar-huge-128k-online",
       stream: true,
       messages: messages,
     });
